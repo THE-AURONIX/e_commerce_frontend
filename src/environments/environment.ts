e0c1af1193170e40a3f44d757fecf887dd0d1850
Aditya-2004-GitHub <22f3001516@ds.study.iitm.ac.in>
2026-06-23 14:27:57 +0530

    Update backend api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:5000/api'
-  apiUrl: 'https://e-commerce-backend-7t5m.onrender.com'
+  apiUrl: 'https://e-commerce-backend-7t5m.onrender.com/api'
 };
